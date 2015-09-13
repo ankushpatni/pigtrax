1153313f93f13667f5b30ff693793d75e64cef22
Ankush <ankush.patni@gmail.com>
2015-09-13 09:49:14 +0530

    Removal Event

---
+++ src/main/java/com/pigtrax/pigevents/service/RemovalEventExceptSalesServiceImpl.java
@@ -0,0 +1,134 @@
+package com.pigtrax.pigevents.service;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pigtrax.application.exception.PigTraxException;
+import com.pigtrax.pigevents.beans.RemovalEventExceptSalesDetails;
+import com.pigtrax.pigevents.beans.TransportJourney;
+import com.pigtrax.pigevents.dao.interfaces.RemovalEventExceptSalesDetailsDao;
+import com.pigtrax.pigevents.dao.interfaces.TransportJourneyDao;
+import com.pigtrax.pigevents.service.interfaces.RemovalEventExceptSalesService;
+
+@Repository
+public class RemovalEventExceptSalesServiceImpl implements RemovalEventExceptSalesService{
+
+	@Autowired
+	TransportJourneyDao transportJourneyDao;
+	
+	@Autowired
+	RemovalEventExceptSalesDetailsDao removalEventExceptSalesDetailsDao;
+	@Override
+	public RemovalEventExceptSalesDetails getRemovalEventExceptSalesDetailsById(int removalId) throws PigTraxException {
+		try 
+		{
+			RemovalEventExceptSalesDetails removalEventExceptSalesDetails =  removalEventExceptSalesDetailsDao.getRemovalEventExceptSalesDetailsById(removalId);
+			if(null != removalEventExceptSalesDetails)
+			{
+				if(null != removalEventExceptSalesDetails.getTransportJourneyId())
+				{
+					removalEventExceptSalesDetails.setTransportJourney(transportJourneyDao.getTransportJourneyById(removalEventExceptSalesDetails.getTransportJourneyId()));
+				}
+			}
+			return removalEventExceptSalesDetails;
+		}
+		catch (SQLException e)
+		{
+			throw new PigTraxException(e.getMessage(), e.getSQLState());
+		}
+	}
+
+	@Override
+	@Transactional("ptxJTransactionManager")
+	public int addRemovalEventExceptSalesDetails(RemovalEventExceptSalesDetails removalEventExceptSalesDetails)
+			throws PigTraxException 
+	{
+		int returnValue = 0;
+		try
+		{
+			if (null != removalEventExceptSalesDetails.getTransportJourney())
+			{
+				TransportJourney transportJourney = removalEventExceptSalesDetails
+						.getTransportJourney();
+				transportJourney.setUserUpdated(removalEventExceptSalesDetails.getUserUpdated());
+				int transportJourneyId = transportJourneyDao.addTransportJourney(removalEventExceptSalesDetails
+						.getTransportJourney());
+				
+				if(transportJourneyId !=0 )
+				{
+					removalEventExceptSalesDetails.setTransportJourneyId(transportJourneyId);					
+				}
+				else
+				{
+					throw new PigTraxException("Not able to create transport journey",null);
+				}
+				
+			}			
+			returnValue = removalEventExceptSalesDetailsDao.addRemovalEventExceptSalesDetails(removalEventExceptSalesDetails);
+			
+		} 
+		catch (SQLException sqlEx)
+		{
+			if ("23505".equals(sqlEx.getSQLState()))
+			{
+				throw new PigTraxException("GroupId already exists",
+						sqlEx.getSQLState(), true);
+			} 
+			else
+			{
+				throw new PigTraxException("SqlException occured",
+						sqlEx.getSQLState());
+			}
+		}
+		catch (DuplicateKeyException sqlExDup)
+		{
+			throw new PigTraxException("GroupId already exists",
+						null, true);
+		}
+		return returnValue;
+	}
+
+	@Override
+	public int updateFeedEventDeupdateRemovalEventExceptSalesDetailstail(RemovalEventExceptSalesDetails removalEventExceptSalesDetails)
+			throws PigTraxException {
+		int returnValue = 0;
+		try
+		{
+			if (null != removalEventExceptSalesDetails.getTransportJourney() &&
+					(removalEventExceptSalesDetails.getTransportJourney().getId() == null || removalEventExceptSalesDetails.getTransportJourney().getId() ==0) )
+			{
+				TransportJourney transportJourney = removalEventExceptSalesDetails
+						.getTransportJourney();
+				transportJourney.setUserUpdated(removalEventExceptSalesDetails.getUserUpdated());
+				int transportJourneyId = transportJourneyDao.addTransportJourney(removalEventExceptSalesDetails
+						.getTransportJourney());
+				
+				if(transportJourneyId !=0 )
+				{
+					removalEventExceptSalesDetails.setTransportJourneyId(transportJourneyId);					
+				}
+			}
+			returnValue = removalEventExceptSalesDetailsDao.updateRemovalEventExceptSalesDetails(removalEventExceptSalesDetails);
+		} 
+		catch (SQLException e)
+		{
+			throw new PigTraxException(e.getMessage(), e.getSQLState());
+		}
+		return returnValue;
+	}
+
+	@Override
+	public List<RemovalEventExceptSalesDetails> getRemovalEventExceptSalesDetailsListByRemovalId(
+			RemovalEventExceptSalesDetails salesEventDetails)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/pigtrax/pigevents/service/interfaces/RemovalEventExceptSalesService.java
@@ -0,0 +1,20 @@
+package com.pigtrax.pigevents.service.interfaces;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.pigtrax.application.exception.PigTraxException;
+import com.pigtrax.pigevents.beans.RemovalEventExceptSalesDetails;
+
+public interface RemovalEventExceptSalesService {
+	
+	public RemovalEventExceptSalesDetails getRemovalEventExceptSalesDetailsById(final int id)
+			throws PigTraxException ;
+	
+	int addRemovalEventExceptSalesDetails(final RemovalEventExceptSalesDetails salesEventDetails) throws PigTraxException;
+	
+	int updateFeedEventDeupdateRemovalEventExceptSalesDetailstail(final RemovalEventExceptSalesDetails salesEventDetails) throws PigTraxException;
+	
+	public List<RemovalEventExceptSalesDetails> getRemovalEventExceptSalesDetailsListByRemovalId(final RemovalEventExceptSalesDetails salesEventDetails)
+			throws SQLException ;
+}
