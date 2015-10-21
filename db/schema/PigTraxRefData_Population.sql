443a96be0035bfbc73823cc6ff34031edc1988f1
Vidya R <vidyar2002@yahoo.com>
2015-10-21 17:18:10 +0530

    client encoding UTF8 removed

--- db/schema/PigTraxRefData_Population.sql
+++ db/schema/PigTraxRefData_Population.sql
@@ -1,4 +1,3 @@
-SET client_encoding = 'UTF8';
 --Roles
 INSERT INTO pigtraxrefdata."RoleType" ("fieldCode","fieldDescription","lastUpdated","userUpdated") VALUES (1,'PigTrax SuperAdmin User',current_timestamp,'pigtraxadmin');
 INSERT INTO pigtraxrefdata."RoleType" ("fieldCode","fieldDescription","lastUpdated","userUpdated") VALUES (2,'PigTrax Data Config Mgr',current_timestamp,'pigtraxadmin');
